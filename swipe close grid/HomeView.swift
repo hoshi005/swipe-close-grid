0b0b035acda4b54eb1d91701d3d57454f05dbcbb
Susumu Hoshikawa <hoshi005@me.com>
2021-01-31 11:21:15 +0900

    first commit

---
+++ swipe close grid/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  swipe close grid
+//
+//  Created by Susumu Hoshikawa on 2021/01/31.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ swipe close grid/swipe_close_gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipe_close_gridApp.swift
+//  swipe close grid
+//
+//  Created by Susumu Hoshikawa on 2021/01/31.
+//
+
+import SwiftUI
+
+@main
+struct swipe_close_gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
